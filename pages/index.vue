42fea1f3991562b143b3e6a6cdd6511fc822c26b
Keyurrr14 <keyurrathod9920@gmail.com>
2024-05-20 12:32:39 +0530

    animation added to home and about

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <div class="w-full h-screen bg-zinc-800">
-
+  <div class="p-10 w-full h-screen bg-zinc-800 text-white">
+    <center>
+      <transition name="fade1" >
+        <h1 v-if="showText" class="text-5xl m-10">This is Home Page</h1>
+      </transition>
+      <transition name="fade">
+        <p v-if="showText" class="text-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+          deleniti architecto hic ea rem, enim doloribus ducimus similique,
+          labore distinctio sit tenetur error laudantium! Enim architecto optio
+          exercitationem magnam autem unde, consequatur, delectus quisquam ea
+          quas dicta! Ex officia quaerat modi minima voluptate repellat incidunt
+          maiores nihil? Soluta, ex doloremque.
+        </p>
+      </transition>
+    </center>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
 
-<style scoped></style>
+const showText = ref(false);
+
+onMounted(() => {
+  showText.value = true;
+});
+</script>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s ease;
+}
+
+.fade1-enter-from,
+.fade1-leave-to {
+  opacity: 0;
+}
+.fade1-enter-to,
+.fade1-leave-from {
+  opacity: 1;
+}
+.fade1-enter-active,
+.fade1-leave-active {
+  transition: opacity 2s ease;
+}
+</style>
